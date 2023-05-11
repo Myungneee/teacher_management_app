7dbed03925e518909d6dbebb5e335021fce0e05a
Myung <shinei302010@gmail.com>
2023-05-11 10:01:36 +0700

    gv2

---
+++ delete/delete.js
@@ -0,0 +1,17 @@
+// const deleteButton = document.querySelector("#delete-btn");
+// const overlay = document.querySelector(".overlay");
+// const noButton = document.querySelector("#no-btn");
+// const yesButton = document.querySelector("#yes-btn");
+
+// deleteButton.addEventListener("click", function() {
+//   overlay.style.display = "block";
+// });
+
+// noButton.addEventListener("click", function() {
+//   overlay.style.display = "none";
+// });
+
+// yesButton.addEventListener("click", function() {
+//   // code xử lý xóa giáo viên ở đây
+//   overlay.style.display = "none";
+// });
---
+++ thongtingv/thongtingv.js
@@ -0,0 +1,7 @@
+
+JavaScript:
+
+const backButton = document.querySelector(".class-info button");
+backButton.addEventListener("click", function() {
+  window.history.back();
+});
